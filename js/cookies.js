b9e99ff8409229c0df480d94b8245018c5f88057
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-18 14:43:07 +0200

    create setCookie , deleteCookie

--- js/cookies.js
+++ js/cookies.js
@@ -14,3 +14,35 @@ document.cookie = 'user=Roman; max-age=7200; secure; samesite=lax';
 
 console.log(document.cookie);
 
+document.cookie = 'httpOnly';
+
+
+const setCookies = (name, value, options = {}) => {
+  options = {
+    path: '/',
+    ...options,
+  };
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+  let updatedCookie =
+    encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    for (let optionKey in options) {
+    updatedCookie += '; ' + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += '=' + optionValue;
+    }
+  }
+  document.cookie = updatedCookie;
+};
+
+setCookies('user', 'Roman', { secure: true, 'max-age': 3600 });
+
+const deleteCookie = (name) => {
+  setCookies(name, '', {
+    'max-age': -1,
+  });
+};
+
+console.log(document.cookie);
