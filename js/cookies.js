d6610f4930e011b304ac3f7b3a13ef355e1dde40
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-18 12:55:39 +0200

    cookie methods

--- js/cookies.js
+++ js/cookies.js
@@ -1,3 +1,16 @@
 'use strict';
 
+document.cookie = 'user=Roman';
+
+let name = 'user name';
+let value = 'Roman Chaban';
+
+document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+
+document.cookie = 'user=John; path=/; expires=Sun, 19 2024 domain=site.com';
 console.log(document.cookie);
+
+document.cookie = 'user=Roman; max-age=7200; secure; samesite=lax';
+
+console.log(document.cookie);
+
